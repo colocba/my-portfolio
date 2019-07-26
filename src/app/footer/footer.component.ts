221c183ebbc62ec5bb1c2dafbe66fdf809194988
amirbaum <colocba@gmail.com>
2019-07-26 18:46:45 +0300

    Styled footer and added effects in header

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,7 +9,7 @@ export class FooterComponent implements OnInit {
 
   subtitle: string;
   constructor() {
-    this.subtitle = "I'm Amir Baum, a Computer Science graduate focusing on web development. Passionate about web development, specially with new framework technologies as Angular or React. Let me know your ideas and I'll make them live!"
+    this.subtitle = "I'm Amir Baum, a Computer Science graduate focusing on web development. Passionate about web development, specially with new framework technologies as Angular or React. All my projects are made from scratch, no templates have been used here. Let me know your ideas and I'll make them live!"
   }
 
   ngOnInit() {
